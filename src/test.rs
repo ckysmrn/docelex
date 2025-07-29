73d315d4602530d3ae99cbba6d8b54f41ead107b
amsitlab <dezavue3@gmail.com>
2025-07-30 00:07:14 +0700

    add: src/test.zig

---
+++ src/test.rs
@@ -0,0 +1,24 @@
+const Cursor = @import("Cursor.zig");
+const std = @import("std");
+const code = 
+    \\let a = 1;
+    \\let b = 2;
+    ;
+
+
+pub fn is_ident_head(c: u8) bool {
+    return std.ascii.isAlphabetic(c) or c == '_';
+}
+
+
+test "Cursor::basic" {
+    var cursor = Cursor.new(code);
+    std.debug.print("pos: {d}", .{cursor.getTokenLen()});
+    cursor.moveWhile(is_ident_head);
+    
+    std.debug.print("pos: {d}", .{cursor.getTokenLen()});
+    cursor.resetTokenLen();
+    std.debug.print("len_remaining: {d}", .{cursor.len_remaining});
+}
+
+
